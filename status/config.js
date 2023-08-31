53963aa26c8eac1d86eea387f7f6f29fe14cc92b
PYLXU <104706823+PYLXU@users.noreply.github.com>
2023-08-31 08:40:53 +0800

    Update config.js

--- status/config.js
+++ status/config.js
@@ -1,15 +1,15 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '瑞思服务状态',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm795191092-56695b9932a3f2a8c6296761',
+    'm795191104-d122c4fc7b8bd17dcf89c19a',
+    'm795191096-f391e485e1cd98e509c782ed',
+    'm795191100-f4eaaa64ebd6d588e8287ef1',
   ],
 
   // 日志天数
@@ -21,16 +21,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '状态页',
+      url: 'https://g.3r60.top/status'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: '官网',
+      url: 'https://3r60.top'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '博客',
+      url: 'https://blog.3r60.top/'
     },
   ],
 };
