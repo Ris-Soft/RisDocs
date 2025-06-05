a04b7198ae8d21d7b24c0fbd717f5fb8bec546ea
PYLXU <104706823+PYLXU@users.noreply.github.com>
2025-06-05 15:38:22 +0000

    Deploying to main from @ PYLXU/pylxu.github.io@cd8f4d97e9b4931e69c47621ea90c22ff7513711 🚀

--- docs/.vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"瑞思文档\",\"description\":\"腾瑞思智官方文档站点\",\"head\":[[\"link\",{\"rel\":\"icon\",\"type\":\"image/ico\",\"href\":\"https://api.3r60.top/icons/user.ico\"}]],\"locales\":{}}")
--- docs/notes/demo/foo.md
+++
@@ -1,7 +0,0 @@
----
-title: foo
-createTime: 2025/06/05 22:53:57
-permalink: /demo/ny4w2yxn/
----
-
-[bar](./bar.md)
