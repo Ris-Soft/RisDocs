da8139e7b678aaefd9750d12e3a01ce0a41fdea0
PYLXU <e-pylxu@outlook.com>
2025-06-05 22:34:13 +0800

    feat: 添加VuePress

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,25 @@
+import * as clientConfig0 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-hea_2852394b3ff024a0ad09608bbc80521d/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-to_326a0a3c270f16400fdeec0257de3667/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@_f89ca12e7f95bdf29277b1e1a83f62da/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-h_6ae7662c8eefc05e240d55ca21736bfc/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from 'D:/RisDocs/vuepress-starter/docs/.vuepress/.temp/git/config.js'
+import * as clientConfig5 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoo_a159466d6597df154a716af78ce69e0c/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig6 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@_66be6aee2e69049ca3ae09d664baabf4/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig7 from 'D:/RisDocs/vuepress-starter/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig8 from 'D:/RisDocs/vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig9 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data_4078affc4ed687ef28a254da13d64fd7/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig10 from 'D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0_42814d3c4b8a6e4052c0f804281b2dd2/node_modules/@vuepress/theme-default/lib/client/config.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+].map((m) => m.default).filter(Boolean)
---
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import { CodeTabs } from "D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-t_bfd632761e8391bac32718f122cb0d14/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-t_bfd632761e8391bac32718f122cb0d14/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "D:/RisDocs/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-t_bfd632761e8391bac32718f122cb0d14/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
