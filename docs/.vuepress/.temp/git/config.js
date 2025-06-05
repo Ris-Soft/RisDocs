3cafcbcba884d3386b72e3bd0e1ddb0c4b910976
PYLXU <e-pylxu@outlook.com>
2025-06-05 23:36:25 +0800

    refactor: 构建

--- docs/.vuepress/.temp/git/config.js
+++ docs/.vuepress/.temp/git/config.js
@@ -1,7 +1,7 @@
-
+import { GitContributors } from "D:/RisDocs/pylxu.github.io/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_e757439b978208a9c6998db044f1335c/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
 
 export default {
   enhance: ({ app }) => {
-
+    app.component("GitContributors", GitContributors);
   },
 };
--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"瑞思文档\",\"description\":\"腾瑞思智官方文档站点\",\"head\":[[\"link\",{\"rel\":\"icon\",\"type\":\"image/ico\",\"href\":\"https://api.3r60.top/icons/user.ico\"}]],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
