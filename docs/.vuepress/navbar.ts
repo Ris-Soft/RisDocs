21e00566bf22a32f2ac3381283fe168074c451bf
PYLXU <e-pylxu@outlook.com>
2025-06-06 20:35:04 +0800

    feat: 添加关于页面

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,7 +9,8 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export default defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '用户协议', link: '/terms' },
-  { text: '博客', link: '/blog/' },
+  { text: '关于', link: '/about' },
+  // { text: '博客', link: '/blog/' },
   // { text: '标签', link: '/blog/tags/' },
   // { text: '归档', link: '/blog/archives/' },
   // {
---
+++ docs/about.md
@@ -0,0 +1,19 @@
+---
+title: about
+createTime: 2025/06/06 20:28:32
+permalink: /article/vtkd3rcc/
+---
+# 关于我们
+
+## 官方社群
+
+[RisChat](https://qm.qq.com/q/3swohB3zsQ)
+
+## 开发团队
+
+- PYLXU 核心开发者
+- 风吟残歌等人 开发者
+
+## 合作关系
+
+与 争翼再创公司 为合作关系，为当前域名模板所有者
